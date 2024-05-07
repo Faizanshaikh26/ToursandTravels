2e012e53b34e00d31c9071d8a3b86ea48ea13c00
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-05-07 17:17:04 +0530

    images

--- src/Components/Choose.jsx
+++ src/Components/Choose.jsx
@@ -13,7 +13,7 @@ function Choose() {
         </p>
         <div className="display__grid">
           <div className="display__card grid-1">
-            <img src="src/Images/1.jpg" alt="grid" />
+            <img src="https://plus.unsplash.com/premium_photo-1661636737716-aecafc3bdb4d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG91cnMlMjBhbmQlMjB0cmF2ZWxzfGVufDB8fDB8fHww" alt="grid" />
           </div>
           <div className="display__card">
           <i className="fa-solid fa-earth-americas"></i>
@@ -21,10 +21,10 @@ function Choose() {
             <p>Fuel your passion for adventure and discover new horizons</p>
           </div>
           <div className="display__card">
-            <img src="src/Images/2.jpg" alt="grid" />
+            <img src="https://plus.unsplash.com/premium_photo-1679088033235-3c029d36bd04?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHRvdXJzJTIwYW5kJTIwdHJhdmVsc3xlbnwwfHwwfHx8MA%3D%3D" alt="grid" />
           </div>
           <div className="display__card">
-            <img src="src/Images/4.jpg" alt="grid" />
+            <img src="https://plus.unsplash.com/premium_photo-1664358190246-60cf5076e833?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fHRvdXJzJTIwYW5kJTIwdHJhdmVsc3xlbnwwfHwwfHx8MA%3D%3D" alt="grid" />
           </div>
           <div className="display__card">
           <i className="fa-solid fa-location-dot"></i>
