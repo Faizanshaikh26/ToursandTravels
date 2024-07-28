94591d491615fe14515ef1a568f354a2a7c0a2ee
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-07-28 12:11:02 +0530

    links-done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../Styles/Navbar.css";
 import { Link } from "react-scroll";
+import { NavLink } from "react-router-dom";
 function Navbar() {
   const [Openmenu, setOpenmenu] = useState(false);
   const Menuopen = () => {
@@ -9,7 +10,7 @@ function Navbar() {
   return (
     <>
       <div className="Header">
-        <Link to="">
+        <Link to="/">
           <img src="" alt="Logo" />
         </Link>
         <div>
@@ -20,52 +21,95 @@ function Navbar() {
               className="fa-sharp  fa-solid fa-xmark"
             ></i>
             <li>
-              <Link to="Home"  spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={500} >Home</Link>
+              <Link
+                to="Home"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="expertice"  spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={500} >Expertice</Link>
+              <Link
+                to="expertice"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Expertice
+              </Link>
             </li>
             <li>
-              <Link to="services"  spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={500} >Services</Link>
+              <Link
+                to="services"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Services
+              </Link>
             </li>
             <li>
-              <Link to="choose"  spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={500} >Choose Us</Link>
+              <Link
+                to="choose"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Choose Us
+              </Link>
             </li>
             <li>
-              <Link to="about"  spy={true} 
-      smooth={true} 
-      offset={50} 
-      duration={500} >About Us</Link>
+              <Link
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                About Us
+              </Link>
             </li>
             <li>
-              <Link to="expereince"  spy={true} 
-      smooth={true} 
-      offset={-150} 
-      duration={500} >Expereince</Link>
+              <Link
+                to="expereince"
+                spy={true}
+                smooth={true}
+                offset={-150}
+                duration={500}
+              >
+                Expereince
+              </Link>
             </li>
             <li>
-              <Link to="gallery"  spy={true} 
-      smooth={true} 
-      offset={-150} 
-      duration={500} >Gallery</Link>
+              <Link
+                to="gallery"
+                spy={true}
+                smooth={true}
+                offset={-150}
+                duration={500}
+              >
+                Gallery
+              </Link>
             </li>
             <li>
-              <Link to="testnomial"  spy={true} 
-      smooth={true} 
-      offset={-100} 
-      duration={500} >Testnomial</Link>
+              <Link
+                to="testnomial"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Testnomial
+              </Link>
+            </li>
+            <li>
+              <NavLink to='/contact'><i class="fa-solid fa-message"></i></NavLink>
             </li>
           </ul>
         </div>
